46a33854a6224b03e9cb826d2a4743ddb075ddbd
Yogesh786Chouhan <129957831+Yogesh786Chouhan@users.noreply.github.com>
2024-07-18 17:49:48 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -46,31 +46,3 @@ app.use(errorMiddleware);
 module.exports = app;
 
 
-
-// EMAIL_SENDER = kishorekumar8273@gmail.com
-
-// SMTP_MAIL = jannie58@ethereal.email
-
-// SMTP_PASSWORD = 4Q4DgjSBg1HwREHaSn
-
-// SMTP_HOST = smtp.ethereal.email
-
-// SMTP_PORT = 587
-
-// CLOUDINARY_NAME = dcee1h9mq
-
-// CLOUDINARY_API_KEY = 241559156539496
-
-// CLOUDINARY_API_SECRET = p4D9ItYjc9ucVJLNbvSM4JXVPH4
-
-// STRIPE_API_KEY = pk_test_51PRvWSSA3GdxVw3t8k0VtO6Fgpbi0PTu9xyTJ0GzRjmyoWUR2A6T2qoFUjaEu6QF5owPVRxMkeWO35sr8drYlB8q00cnRxBUj7
-
-// STRIPE_SECRET_KEY = sk_test_51PRvWSSA3GdxVw3t7uxPEfSr06KFd16hjwgjqNT9tVhja4cJnss5YgoouxmvYnoqTpKu07owH07WNEdFrWzFWLsb003Fz68r17
-// PORT = 4000 
-// DB_URI = "mongodb+srv://yogeshchouhan074358:V2zx7FQJpzcXNkNe@ecommerce.pbecgev.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=Ecommerce"
-
-// JWT_SECRET = ghdksfdkgkhfsdf565eq4KHJHDFAVKJLJjkhdfd658
-// JWT_EXPIRE = 5d
-// COOKIE_EXPIRE= 5
-
-// NODE_ENV = PRODUCTION
